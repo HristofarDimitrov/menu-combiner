4de5ef28f2d09348c630aaf5a2af6db941bf5bcd
HristofarDimitrov <h.dimitrov@bitpioneers.com>
2021-11-22 22:30:13 +0200

    app fundamet init

--- menu-combiner/src/app/app.component.ts
+++ menu-combiner/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetDataService } from './services/getData.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'menu-combiner';
+
+  constructor(private getData: GetDataService) {}
+
+  public data: any = this.getData.getPivniceUcapaData();
+
+  ngOnInit() {
+    // this.getData.getPivniceUcapaData().subscribe((data) => {
+    //   console.log(data);
+    // });
+  }
 }
